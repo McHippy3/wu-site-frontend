ce83fb88f17d93138ac66bea81b544de45908b0b
David Wu <30442209+DW@users.noreply.github.com>
2020-06-04 20:45:47 -0500

    whiter design

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import './home.css'
 
 function Home(props) {
-    return <h1 className="heading">Hello</h1>
+  return (
+    <div className="section" id="home">
+      <div id="heading-container">
+      <h3 className="heading">Hello there</h3>
+      <h3 className="heading">My name is David Wu</h3>
+      <h3 className="heading">Welcome to my website</h3>
+      </div>
+    </div>
+  )
 }
 
 export default Home
